669912126b0f5fc480da0cc673af77b668678a7f
unknown <turnern1@PSLW24629.b2b.regn.net>
2016-02-09 09:17:38 +0000

    plugin resource

--- manifests/server/plugin.pp
+++ manifests/server/plugin.pp
@@ -1,29 +1,74 @@
-class teamcity::server::plugin(
+# Define: teamcity::server::plugin
+#
+# This is a defined type to a teamcity plugin
+#
+# == Parameters
+# [*ensure*]
+#   Allows creation or removal of swapspace and the corresponding file.
+# [*swapfile*]
+#   Location of swapfile, defaults to /mnt
+# [*plugin_url*]
+#   web location of plugin to dowload 
+# [*plugin_zip_file*]
+#   file name to store plugin in teamcity plugin directory
+# [*wget_opts*]
+#   wget opts to use  
+#
+# == Example
+#
+#    class { 'teamcity::server::plugin':
+#      plugin_url       => 'http://teamcity.jetbrains.com/guestAuth/repository/download/bt434/.lastSuccessful/jonnyzzz.node.zip',
+#      plugin_zip_file  => 'jonnyzzz.node.zip',
+#      wget_opts        => "-e use_proxy=yes -e http_proxy=http://mycompanyproxy.com:3128",
+#      require          => Class['teamcity::server'],
+#   }
+#
+#
+define teamcity::server::plugin (
+  $ensure           = 'present',
   $plugin_url       = '',
   $plugin_zip_file  = '',
-  $wget_opts        = '',)
+  $wget_opts        = ''
+)
 {
+  # Parameter validation
+  validate_re($ensure, ['^absent$', '^present$'], "Invalid ensure: ${ensure} - (Must be 'present' or 'absent')")
 
-  exec { 'download teamcity plugin':
-    command => "wget ${wget_opts} \"${plugin_url}\"",
-    creates => "${teamcity::server::plugin_dir}/${plugin_zip_file}",
-    cwd     => $teamcity::server::plugin_dir,
-    notify  => Exec['restart teamcity service'],
-    timeout => 0
-  }
+  if $ensure == 'present' {
+    exec { 'download teamcity plugin':
+      command => "wget ${wget_opts} \"${plugin_url}\"",
+      creates => "${teamcity::server::plugin_dir}/${plugin_zip_file}",
+      cwd     => $teamcity::server::plugin_dir,
+      notify  => Exec['restart teamcity service'],
+      timeout => 0
+    }
   
-  exec { 'set ownership teamcity plugin':
-    command => "chown ${teamcity::server::user}:${teamcity::common::group} \"${teamcity::server::plugin_dir}/${plugin_zip_file}\"",
-    cwd     => $teamcity::server::plugin_dir,
-    require => Exec['download teamcity plugin'],
-    timeout => 0
-  }
+    exec { 'set ownership teamcity plugin':
+      command => "chown ${teamcity::server::user}:${teamcity::common::group} \"${teamcity::server::plugin_dir}/${plugin_zip_file}\"",
+      cwd     => $teamcity::server::plugin_dir,
+      require => Exec['download teamcity plugin'],
+      timeout => 0
+    }
   
-  exec { 'restart teamcity service':
-    command     => "service ${teamcity::server::service} restart",
-    cwd         => $teamcity::server::home_dir,
-    refreshonly => true,
-    timeout     => 0
+    exec { 'restart teamcity service':
+      command     => "service ${teamcity::server::service} restart",
+      cwd         => $teamcity::server::home_dir,
+      refreshonly => true,
+      timeout     => 0
+    }
   }
+  elsif $ensure == 'absent' {
+    file { "${teamcity::server::plugin_dir}/${plugin_zip_file}":
+      ensure  => absent,
+      backup  => false,
+    }
+    
+    exec { 'restart teamcity service to remove plugin':
+      command     => "service ${teamcity::server::service} restart",
+      cwd         => $teamcity::server::home_dir,
+      refreshonly => true,
+      timeout     => 0
+    }
+  }
+
 }
-  
